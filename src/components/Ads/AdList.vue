bba7449ce61749775ae79c1fefa6995ace97526e
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-05-06 19:54:44 +0300

    add ui componemt AdList

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,41 @@
 <template>
   <v-container>
-    <v-layout row>
+    <v-layout>
       <v-flex xs12 sm6 offset-sm3>
         <h1 class="mb-3">My ads</h1>
+
+        <v-card
+            class="mx-auto mb-8"
+            max-width="344"
+            v-for="ad of ads"
+            :key="ad.id"
+            elevation="12"
+          >
+            <v-img
+              :src="ad.imageSrc"
+              height="200px"
+            ></v-img>
+
+            <v-card-title>
+              {{ ad.title }}
+            </v-card-title>
+
+            <v-card-subtitle>
+              {{ ad.description }}
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="purple"
+                text
+                :to="'/ad/' + ad.id"
+              >
+                Open
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,6 +45,29 @@
 export default {
   data () {
     return {
+      ads: [
+        {
+          title: 'squirrel',
+          description: 'Hello i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '10'
+        },
+        {
+          title: 'squirrel',
+          description: 'Hello i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '10'
+        },
+        {
+          title: 'squirrel',
+          description: 'Hello i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '10'
+        }
+      ]
     }
   }
 }
